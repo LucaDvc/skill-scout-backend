adf1363b0bbf2c0b090be4ce5f80916066154b13
luka_dvc <lukadvc3@gmail.com>
2023-04-08 12:26:24 +0300

    created login (jwt) and register rest api endpoints

---
+++ users/api/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+from users.models import User
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password1 = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'first_name', 'last_name', 'password1', 'password2']
+        extra_kwargs = {
+            'first_name': {'required': True},
+            'last_name': {'required': True}
+        }
+
+    def validate(self, attrs):
+        if attrs['password1'] != attrs['password2']:
+            raise serializers.ValidationError({'register_error': 'Password fields do not match'})
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            validated_data['email'],
+            validated_data['password1'],
+            validated_data['first_name'],
+            validated_data['last_name']
+        )
+        user.save()
+        return user
